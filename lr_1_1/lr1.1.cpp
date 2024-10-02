80720c1cb34ee32324349c600ff5bcef3b4cfa53
guse95 <saveliy.gusev.05@list.ru>
2024-10-03 00:20:47 +0300

    дописан отлов ошибок в lr1.1

--- lr_1_1/lr1.1.cpp
+++ lr_1_1/lr1.1.cpp
@@ -7,28 +7,74 @@
 
 enum ret_type_t {
     SUCCSESS,
-    ERROR
+    ERROR_NO_VALUE,
+    ERROR_NEGATIVE_VALUE,
+    ERROR_NOT_NUMBER,
+    ERROR_TOO_LONG_STR,
+    ERROR_NUMBER_OUT_OF_RANGE
 };
 
 typedef ret_type_t(*callback)(char*);
 
 
-int is_number(const char* s) {
-    if (*s == '\0') return ERROR;
+ret_type_t is_number(const char* s) {
+    if (*s == '\0') return ERROR_NO_VALUE;
 
     while (*s == ' ') s++;
     
     if (*s == '-') s++;
 
-    while (isdigit(*s)) s++;
+    int len = 0;
+    while (isdigit(*s)) {
+        s++;
+        if (len++ > 9) return ERROR_TOO_LONG_STR;
+    }
 
     if (*s == '\0') return SUCCSESS;
-    return ERROR;
+    return ERROR_NOT_NUMBER;
+}
+
+void HandlingError(int code) {
+    switch (code) {
+        case ERROR_NEGATIVE_VALUE:
+            printf("Negative value was entered.\n");
+            break;
+        case ERROR_NO_VALUE:
+            printf("No number was entered.\n");
+            break;
+        case ERROR_NOT_NUMBER:
+            printf("Value isn`t a number.\n");
+            break;
+        case ERROR_TOO_LONG_STR:
+            printf("Too long string was entered.\n");
+            break;
+        case ERROR_NUMBER_OUT_OF_RANGE:
+            printf("Number out of range.\n");
+            break;
+        default:
+            break;
+    }
+}
+
+int Atoi(const char* s) {
+    while (*s == ' ') s++;
+    int flag = 1;
+    if (*s == '-') {
+        flag = -1;
+        s++;
+    }
+    int res = 0;
+    while (*s != '\0') {
+        res = (res * 10) + (*s - '0');
+        s++;
+    }
+    res *= flag;
+    return res;
 }
 
 ret_type_t funcForH(char* argv) {
-    if (is_number(argv)) return ERROR;
-    const int x = abs(atoi(argv));
+    if (ret_type_t code = is_number(argv)) return code;
+    const int x = abs(Atoi(argv));
     int ans = x;
     if (x > 100)
     {
@@ -44,8 +90,10 @@ ret_type_t funcForH(char* argv) {
 }
 
 ret_type_t funcForP(char* argv) {
-    if (is_number(argv)) return ERROR;
-    const int x = atoi(argv);
+    if (ret_type_t code = is_number(argv)) {
+        return code;
+    }
+    const int x = Atoi(argv);
     if (x <= 1)
     {
         printf("X isn`t composite or simple.\n");
@@ -63,39 +111,34 @@ ret_type_t funcForP(char* argv) {
 }
 
 ret_type_t funcForS(char* argv) {
-    if (is_number(argv)) return ERROR;
-    int x = atoi(argv);
-    if (x < 1) return ERROR;
-    char* res = (char*)malloc(sizeof(x) / sizeof(int));
-    memset(res, '\0', sizeof(x) / sizeof(int));
-    int len = 0;
-    while (x > 0)
-    {
-        res[len] =  x % 16;
-        x /= 16;
-        ++len;
+    if (ret_type_t code = is_number(argv)) return code;
+    int x = Atoi(argv);
+    if (x < 1) return ERROR_NEGATIVE_VALUE;
+    int base = 16, res = 0;
+    char buf[BUFSIZ], *pb = buf + BUFSIZ - 1, r;
+    *pb-- = 0;
+    while (x) {
+        *pb-- = ((r = x % base) > 9) ? r - 10 + 'A' : r + '0';
+        x /= base;
     }
-    --len;
-    while (len >= 0) {
-        printf("%X ", res[len]);
-        --len;
-    }
+    while (*++pb)
+        printf("%c ", *pb);
     printf("\n");
-    free(res);
+
     return SUCCSESS;
 }
 
 ret_type_t funcForE(char* argv) {
-    if (is_number(argv)) return ERROR;
-    const int x = atoi(argv);
-    if (x < 1 || x > 10) return ERROR;
+    if (ret_type_t code = is_number(argv)) return code;
+    const int x = Atoi(argv);
+    if (x < 1 || x > 10) return ERROR_NUMBER_OUT_OF_RANGE;
     for (int base = 1; base <= 10; base++)
     {
-        int ans = 1;
+        long long ans = 1;
         for (int _ = 1; _ <= x; _++)
         {
             ans *= base;
-            printf("%-10d", ans);
+            printf("%-11lld", ans);
         }
         printf("\n");
     }
@@ -103,22 +146,22 @@ ret_type_t funcForE(char* argv) {
 }
 
 ret_type_t funcForA(char* argv) {
-    if (is_number(argv)) return ERROR;
-    const int x = atoi(argv);
-    if (x < 1) return ERROR;
-    int summ = (x + 1) * x / 2;
-    printf("%d\n", summ);
+    if (ret_type_t code = is_number(argv)) return code;
+    const long long x = Atoi(argv);
+    if (x < 1) return ERROR_NEGATIVE_VALUE;
+    long long summ = (x % 2 == 0) ? (x / 2) * (x + 1): ((x + 1) / 2) * x;
+    printf("%lld\n", summ);
     return SUCCSESS;
 }
 
 ret_type_t funcForF(char* argv) {
-    if (is_number(argv)) return ERROR;
-    const int x = atoi(argv);
-    if (x < 1) return ERROR;
+    if (ret_type_t code = is_number(argv)) return code;
+    const int x = Atoi(argv);
+    if (x < 1) return ERROR_NEGATIVE_VALUE;
     if (x > 34)
     {
         printf("The factorial exceeds 2^128.\n");
-        return ERROR;
+        return ERROR_NUMBER_OUT_OF_RANGE;
     }
     unsigned long long faq = 1;
     for (int i = 2; i <= x; i++)
@@ -151,14 +194,17 @@ int main(int argc, char* argv[]) {
         int ret = findFlag(argv[i], flags, sizeof(flags) / sizeof(char*));
         if (ret == -1)
         {
-            printf("THIS FLAG DOES NOT EXIST %s", argv[i]);
+            printf("THIS FLAG DOES NOT EXIST %s\n", argv[i]);
             return -1;
         }
         else
         {
             int findCbsInt = ret / 2;
             callback find = cbs[findCbsInt];
-            if (find(argv[i + 1])) return -1; /*Надо проверить возвращаемое значение, корректно ли писать i + 1*/
+            if (int code = find(argv[i + 1])) {
+                HandlingError(code);
+                return -1;
+            }
             i++;
         }
 
--- prog1/main.cpp
+++ prog1/main.cpp
@@ -2,6 +2,51 @@
 
 using namespace std;
 
+enum ret_type_t {
+    SUCCSESS,
+    ERROR_NO_VALUE,
+    ERROR_NEGATIVE_VALUE,
+    ERROR_NOT_NUMBER,
+    ERROR_TOO_LONG_NUMBER,
+    ERROR_NUMBER_OUT_OF_RANGE
+};
+
+typedef ret_type_t(*callback)(char*);
+
+
+ret_type_t is_number(const char* s) {
+    if (*s == '\0') return ERROR_NO_VALUE;
+
+    while (*s == ' ') s++;
+
+    if (*s == '-') s++;
+
+    int len = 0;
+    while (isdigit(*s)) {
+        s++;
+        if (len++ > 10) return ERROR_TOO_LONG_NUMBER;
+    }
+
+    if (*s == '\0') return SUCCSESS;
+    return ERROR_NOT_NUMBER;
+}
+
+int atoi(const char* s) {
+    while (*s == ' ') s++;
+    int flag = 1;
+    if (*s == '-') {
+        flag = -1;
+        s++;
+    }
+    int res = 0;
+    while (*s != '\0') {
+        res = (res * 10) + (*s - '0');
+        s++;
+    }
+    res *= flag;
+    return res;
+}
+
 double comb(double m, double n) {
     double res = 1;
     for (double i = m + 1; i <= n; i += 1) {
@@ -14,9 +59,7 @@ double comb(double m, double n) {
 }
 
 int main(){
-    std::cout << "HELLO WORLD\n";
-    char f[] = {'0','.','2','1' };
-    int n = 2;
-    double m = comb(2, 5);
-    printf("%f", m);
+    char str[] = "12345678987654";
+    std::cout << atoi(str);
+
 }
