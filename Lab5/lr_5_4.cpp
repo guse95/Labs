1c0585d94d2929f2f11b498a68dcd9e216698405
guse95 <saveliy.gusev.05@list.ru>
2024-11-18 22:33:42 +0300

    5.4 operations -> methods

--- Lab5/lr_5_4.cpp
+++ Lab5/lr_5_4.cpp
@@ -51,6 +51,35 @@ public:
         return complex{r, i};
     }
 
+    complex comp_sum(complex& a) {
+        double r, i;
+        r = real + a.real;
+        i = imag + a.imag;
+        return complex{r, i};
+    }
+
+    complex comp_dif (complex& a) const {
+        double r, i;
+        r = real - a.real;
+        i = imag - a.imag;
+        return complex{r, i};
+    }
+
+    complex comp_mult (complex& a) const {
+        double r, i;
+        r = real * a.real - imag * a.imag;
+        i = imag * a.real + a.imag * real;
+        return complex{r, i};
+    }
+
+    complex comp_div (complex& a) const {
+        double r, i, divider;
+        divider = a.real * a.real + a.imag * a.imag;
+        r = (real * a.real + imag * a.imag) / divider;
+        i = (imag * a.real - a.imag * real) / divider;
+        return complex{r, i};
+    }
+
     double module() const {
         double len = sqrt(real * real + imag * imag);
         return len;
@@ -75,7 +104,11 @@ std::ostream& operator << (std::ostream &out, const complex& a) {
 }
 
 int main() {
-    complex a(2, 3);
-    complex b(5, 1);
-    std::cout << a.module();
+    complex a(1.2, 8.4);
+    complex b(5.131, 123.5);
+
+    std::cout << a.comp_div(b) << '\n';
+    std::cout << a.comp_dif(b) << '\n';
+    std::cout << a.comp_sum(b) << '\n';
+    std::cout << a.comp_mult(b) << '\n';
 }
